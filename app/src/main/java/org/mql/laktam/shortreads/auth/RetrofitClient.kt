309207b46c5eda2648bde017a99a47766d885d2d
laktam <laktamlaktamabdellah@gmail.com>
2024-09-19 23:45:27 +0100

    use 10.0.2.2 to connect to host localhost

--- app/src/main/java/org/mql/laktam/shortreads/auth/RetrofitClient.kt
+++ app/src/main/java/org/mql/laktam/shortreads/auth/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://127.0.0.1/short-reads-backend/"
+    private const val BASE_URL = "http://10.0.2.2/short-reads-backend/"
 
     val authApiService: AuthApiService by lazy {
         Retrofit.Builder()
