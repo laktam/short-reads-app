ed77179a52c074ad2bb2e5c73e50937ebd5c36c9
laktam <laktamlaktamabdellah@gmail.com>
2024-09-19 23:10:58 +0100

    add SignupScreen to MainActivity

--- app/src/main/java/org/mql/laktam/shortreads/MainActivity.kt
+++ app/src/main/java/org/mql/laktam/shortreads/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,20 +12,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import org.mql.laktam.shortreads.repositories.AuthRepository
+import org.mql.laktam.shortreads.ui.screens.SignupScreen
 import org.mql.laktam.shortreads.ui.theme.ShortReadsTheme
+import org.mql.laktam.shortreads.viewmodel.AuthViewModel
 
 class MainActivity : ComponentActivity() {
+    private val authViewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             ShortReadsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SignupScreen(viewModel = authViewModel)
             }
         }
     }
@@ -42,6 +44,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ShortReadsTheme {
-        Greeting("Android")
+//        SignupScreen()
     }
 }
