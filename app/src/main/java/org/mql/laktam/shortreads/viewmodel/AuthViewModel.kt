7abaebbe36240f29e35b1637e9794f65840765c9
laktam <laktamlaktamabdellah@gmail.com>
2024-09-19 23:02:43 +0100

    add AuthViewModel to manages the signup logic and expose state to the UI

---
+++ app/src/main/java/org/mql/laktam/shortreads/viewmodel/AuthViewModel.kt
@@ -0,0 +1,30 @@
+package org.mql.laktam.shortreads.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.mql.laktam.shortreads.models.AuthState
+import org.mql.laktam.shortreads.repositories.AuthRepository
+
+//Manages the signup logic and exposes state to the UI
+class AuthViewModel() : ViewModel() {//private val authRepository: AuthRepository
+    private val authRepository = AuthRepository()
+    private val _authState = MutableLiveData<AuthState>()
+    val authState: LiveData<AuthState> = _authState
+
+    fun signup(name: String, email: String, password: String) {
+        viewModelScope.launch {
+            _authState.value = AuthState.Loading
+            try {
+                val user = authRepository.signup(name, email, password)
+                _authState.value = AuthState.Success(user)
+            } catch (e: Exception) {
+                _authState.value = AuthState.Error("Signup failed: ${e.message}")
+            }
+        }
+    }
+}
+
+
