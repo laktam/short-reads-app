50c5e0a29ab658e82bb432a7c4838db7316b5988
laktam <laktamlaktamabdellah@gmail.com>
2024-09-21 00:19:21 +0100

    add login method in authRepository to authenticat the user and get jwt token or error message

--- app/src/main/java/org/mql/laktam/shortreads/repositories/AuthRepository.kt
+++ app/src/main/java/org/mql/laktam/shortreads/repositories/AuthRepository.kt
@@ -2,20 +2,28 @@ package org.mql.laktam.shortreads.repositories
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.mql.laktam.shortreads.auth.LoginRequest
 import org.mql.laktam.shortreads.auth.RetrofitClient
 import org.mql.laktam.shortreads.auth.SignupRequest
+import org.mql.laktam.shortreads.models.LoginResponse
 import org.mql.laktam.shortreads.models.SignupResponse
-import org.mql.laktam.shortreads.models.User
 
 // Handles communication with the backend for authentication.
 class AuthRepository {
     private val authApiService = RetrofitClient.authApiService
 
-    suspend fun login(email: String, password: String): User {
-        // Call to backend API for login (e.g., Retrofit, FirebaseAuth)
-        // Mocking a successful login for demonstration
-        return User(email, "John Doe")
+    suspend fun login(username: String, password: String): LoginResponse {
+        return withContext(Dispatchers.IO) {
+            val response = authApiService.login(LoginRequest(username, password))
+            if (response.isSuccessful) {
+                response.body() ?: throw Exception("Login failed")
+            } else {
+                val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+                throw Exception(errorMessage)
+            }
+        }
     }
+
     // withContext(Dispatchers.IO) used to perform network or I/O operation on a background thread
     suspend fun signup(name: String, email: String, password: String): SignupResponse {
         return withContext(Dispatchers.IO) {
