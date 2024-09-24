bcc50f67dda8494584cf4ee258da0612739f6b10
laktam <laktamlaktamabdellah@gmail.com>
2024-09-25 00:22:28 +0100

    update profile view model to be able to update user data

--- app/src/main/java/org/mql/laktam/shortreads/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/org/mql/laktam/shortreads/viewmodels/ProfileViewModel.kt
@@ -1,6 +1,7 @@
 package org.mql.laktam.shortreads.viewmodels
 
 import android.content.Context
+import android.net.Uri
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,24 +10,80 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import org.mql.laktam.shortreads.auth.TokenManager
 import org.mql.laktam.shortreads.models.User
 import org.mql.laktam.shortreads.repositories.UserRepositoryDefault
+import java.io.File
+import java.io.FileOutputStream
 
-class ProfileViewModel : ViewModel() {
-    private val userRepository = UserRepositoryDefault()
+class ProfileViewModel(private val tokenManager: TokenManager) : ViewModel() {
+    private val userRepository = UserRepositoryDefault(tokenManager)
     private val _user = mutableStateOf<User?>(null)
-    val user: State<User?> get() = _user
+    val user: State<User?> get() = _user // not always the logged in user, it is the last visited profile
+    private var profilePicture: MultipartBody.Part? = null
 
+    var followingCurrentProfile = mutableStateOf(false)
+        private set
 
-    fun loadUser(username: String, context: Context) {
+    var currentUsername = mutableStateOf("")
+        private set
+
+    fun updateCurrentUsername(newUsername: String) {
+        currentUsername.value = newUsername
+    }
+
+    fun updatefollowingCurrentProfile(following: Boolean) {
+        followingCurrentProfile.value = following
+    }
+
+    fun loadUser(username: String) {
         viewModelScope.launch {
-            val token = getTokenFromSharedPreferences(context)
-            _user.value = userRepository.getUserByUsername(username, "Bearer $token")
+            _user.value = userRepository.getUserByUsername(username)
+        }
+    }
+    fun onImageSelected(uri: Uri, context: Context) {
+        val imageFile = uriToFile(uri, context)
+        profilePicture = createMultipartBody(imageFile)
+    }
+
+    fun updateUserProfile( username: String, user: User, onComplete: (username: String) -> Unit) {
+        viewModelScope.launch {
+            try {
+                val profileUpdateResponse = userRepository.updateUserProfile(username, user, profilePicture)
+                tokenManager.saveToken(profileUpdateResponse.token)
+                updateCurrentUsername(profileUpdateResponse.username)
+                loadUser(profileUpdateResponse.username)
+                onComplete(profileUpdateResponse.username)
+            } catch (e: Exception) {
+                println("Error updating profile: ${e.message}")
+                println("Error cause: ${e.cause}")
+                e.printStackTrace()
+            }
         }
     }
 
-    private fun getTokenFromSharedPreferences(context: Context): String? {
-        val sharedPreferences = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
-        return sharedPreferences.getString("jwt_token", null)
+    // Utility function to convert URI to File
+    private fun uriToFile(uri: Uri, context: Context): File {
+        val contentResolver = context.contentResolver
+        val file = File(context.cacheDir, "${System.currentTimeMillis()}.jpg")
+        val inputStream = contentResolver.openInputStream(uri) ?: return file
+        val outputStream = FileOutputStream(file)
+
+        inputStream.copyTo(outputStream)
+        inputStream.close()
+        outputStream.close()
+
+        return file
     }
+
+    // Utility function to create MultipartBody.Part
+    private fun createMultipartBody(file: File): MultipartBody.Part {
+        val requestFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+        return MultipartBody.Part.createFormData("file", file.name, requestFile)
+    }
+
+
 }
