70f1656cbad21ad52ade992b9cc8e187303e2c71
laktam <laktamlaktamabdellah@gmail.com>
2024-09-24 20:11:42 +0100

    add updateUserProfile method to user repository

--- app/src/main/java/org/mql/laktam/shortreads/repositories/UserRepository.kt
+++ app/src/main/java/org/mql/laktam/shortreads/repositories/UserRepository.kt
@@ -1,9 +1,13 @@
 package org.mql.laktam.shortreads.repositories
 
 import android.content.Context
+import okhttp3.MultipartBody
+import org.mql.laktam.shortreads.models.LoginResponse
+import org.mql.laktam.shortreads.models.ProfileUpdateResponse
 import org.mql.laktam.shortreads.models.User
 
 
 interface UserRepository {
-    suspend fun getUserByUsername(username: String, token: String): User
+    suspend fun getUserByUsername(username: String): User
+    suspend fun updateUserProfile(username: String, user: User, profilePicture: MultipartBody.Part?): ProfileUpdateResponse
 }
--- app/src/main/java/org/mql/laktam/shortreads/repositories/UserRepositoryDefault.kt
+++ app/src/main/java/org/mql/laktam/shortreads/repositories/UserRepositoryDefault.kt
@@ -1,19 +1,49 @@
 package org.mql.laktam.shortreads.repositories
 
 
+import okhttp3.MultipartBody
 import org.mql.laktam.shortreads.auth.RetrofitClient
+import org.mql.laktam.shortreads.auth.TokenManager
+import org.mql.laktam.shortreads.models.LoginResponse
+import org.mql.laktam.shortreads.models.ProfileUpdateResponse
 import org.mql.laktam.shortreads.models.User
 
-class UserRepositoryDefault() : UserRepository {
-    private val authApiService = RetrofitClient.authApiService
+class UserRepositoryDefault(private val tokenManager: TokenManager) : UserRepository {
+    private val apiService = RetrofitClient.apiService
 
-    override suspend fun getUserByUsername(username: String, token: String): User {
-        val response =  authApiService.getUser(username, token)
+    override suspend fun getUserByUsername(username: String): User {
+        println("username in UserRepository $username")
+        println("token to getusername in repo ${tokenManager.getToken()}")
+        val response =  apiService.getUser(username, "Bearer ${tokenManager.getToken()}")
         if (response.isSuccessful) {
             return response.body() ?: throw Exception("User not found")
         }else{
             val errorMessage = response.errorBody()?.string() ?: "Unknown error"
             throw Exception(errorMessage)
+        }
+    }
+
+    override suspend fun updateUserProfile(username: String, user: User, profilePicture: MultipartBody.Part?): ProfileUpdateResponse {
+        val token = tokenManager.getToken() ?: throw Exception("Token is missing")
+        if(profilePicture != null){
+            val response1 =  apiService.updateUserProfileImage(username, profilePicture, "Bearer $token")
+        }
+        val response2 =  apiService.updateUserProfile(username,user, "Bearer $token")
+//        if(response2.isSuccessful){
+//            return response2.body() ?:throw Exception("Profile not updated")
+//        }else{
+//            val errorMessage = response2.errorBody()?.string() ?: "Unknown error"
+//            throw Exception(errorMessage)
+//        }
+        if (response2.isSuccessful) {
+            val body = response2.body()
+            if (body != null) {
+                return ProfileUpdateResponse(body.token, body.username)
+            } else {
+                throw Exception("Response body is null")
+            }
+        } else {
+            throw Exception("Update failed: ${response2.errorBody()?.string()}")
         }
     }
 
