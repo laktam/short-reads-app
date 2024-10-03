a7c6c873282bebaaef0b2911d3f127af7e48c228
laktam <laktamlaktamabdellah@gmail.com>
2024-10-03 15:43:51 +0100

    add new post screen

---
+++ app/src/main/java/org/mql/laktam/shortreads/ui/screens/NewPostScreen.kt
@@ -0,0 +1,10 @@
+package org.mql.laktam.shortreads.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun NewPostScreen(navController: NavController) {
+    Text("new post screen")
+}
