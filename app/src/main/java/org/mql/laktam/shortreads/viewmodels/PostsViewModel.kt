ffc02ed39ff6fa40ddbb189b6ad910c8f883dfa4
laktam <laktamlaktamabdellah@gmail.com>
2024-10-09 20:09:15 +0100

    add method to load more posts when scrolling to posts view model

--- app/src/main/java/org/mql/laktam/shortreads/viewmodels/PostsViewModel.kt
+++ app/src/main/java/org/mql/laktam/shortreads/viewmodels/PostsViewModel.kt
@@ -4,17 +4,37 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import okhttp3.MultipartBody
 import org.mql.laktam.shortreads.auth.TokenManager
+import org.mql.laktam.shortreads.models.Post
 import org.mql.laktam.shortreads.repositories.PostRepositoryDefault
 import org.mql.laktam.shortreads.utils.ImageMultipartUtils
 import java.net.URI
 
 class PostsViewModel(private val tokenManager: TokenManager): ViewModel() {
-    val postRepository = PostRepositoryDefault(tokenManager)
+    private val postRepository = PostRepositoryDefault(tokenManager)
     private var backgroundImage: MultipartBody.Part? = null
 
+    private val _posts = MutableStateFlow<List<Post>>(emptyList())
+    val posts: StateFlow<List<Post>> get() = _posts
+
+    private var page = 0
+    private val pageSize = 5
+    private var isLastPage = false
+
+    fun loadMorePosts(username: String) {
+        if (isLastPage) return
+
+        viewModelScope.launch {
+            val response = postRepository.getPosts(username, page, pageSize)
+            _posts.value += response.content
+            page++
+            isLastPage = response.last
+        }
+    }
 
     fun onImageSelected(uri: Uri, context: Context) {
         val imageMultipartUtils = ImageMultipartUtils(context);
