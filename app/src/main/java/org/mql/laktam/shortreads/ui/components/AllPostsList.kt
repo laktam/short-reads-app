54fc86ecd7953a08b941e294bc05c3048f8f2f8f
laktam <laktamlaktamabdellah@gmail.com>
2024-10-10 15:48:34 +0100

    use lazycolumn in profile screen instead of AllPostsList composable

--- app/src/main/java/org/mql/laktam/shortreads/ui/components/AllPostsList.kt
+++ app/src/main/java/org/mql/laktam/shortreads/ui/components/AllPostsList.kt
@@ -1,32 +1,46 @@
 package org.mql.laktam.shortreads.ui.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.mql.laktam.shortreads.models.Post
 import org.mql.laktam.shortreads.viewmodels.PostsViewModel
 
 @Composable
 fun AllPostsList(postViewModel: PostsViewModel, username: String) {
 
-    // Collect the posts StateFlow
-    val posts by postViewModel.posts.collectAsState()
-
-    LazyColumn  {
-        items(posts) { post ->
-            TextPostItem(post)
-        }
-
-        item {
-            LaunchedEffect (Unit) {
-                postViewModel.loadMorePosts(username)
-            }
-        }
-    }
+//    // Collect the posts StateFlow
+//    val posts by postViewModel.posts.collectAsState()
+//    val isLastPage by postViewModel.isLastPage.collectAsState()
+//
+//    LazyColumn  {
+//        items(posts) { post ->
+//            TextPostItem(post)
+//        }
+//
+////        item {
+////            LaunchedEffect (Unit) {
+////                postViewModel.loadMorePosts(username)
+////            }
+////        }
+//        item {
+//            if (!isLastPage) {
+//                CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+//
+//                // Trigger loading more posts when scrolled to the end
+//                LaunchedEffect(Unit) {
+//                    postViewModel.laodMore(username)
+//                }
+//            }
+//        }
+//    }
 }
 
 @Composable
